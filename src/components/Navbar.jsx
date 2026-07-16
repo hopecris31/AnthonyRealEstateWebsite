bc4a9586063c9a4d282fe3a90a62896b1716cb0f
hopecris31 <hope.crisafi@gmail.com>
2026-07-15 22:49:25 -0400

    submenu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,39 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../assets/CrisafiRealEstateFullLogo.png'
 
 function Navbar () {
+    const [isResourcesOpen, setIsResourcesOpen] = useState(false)
+
     return (
-        <nav>
+        <nav className="flex items-center justify-between px-8 py-4">
             <Link to="/">
-                <img src={logo} alt="Anthony Real Estate Logo" />
+                <img src={logo} alt="Crisafi Realty Logo" className="h-28 w-auto"/>
             </Link>
 
-            <div>
+            <div className="flex items-center gap-16 text-xl font-medium">
                 <Link to="/buy">Buy</Link>
                 <Link to="/sell">Sell</Link>
                 <Link to="/rent">Rent</Link>
                 <Link to="/about">About</Link>
-                <div>
-                    Resources
+
+                <div className="relative" onMouseEnter={() => setIsResourcesOpen(true)}
+                     onMouseLeave={() => setIsResourcesOpen(false)}>
+                    <button className="cursor-pointer">Resources</button>
+
+                    {isResourcesOpen && (
+                        <div className="absolute top-full left-0 flex flex-col bg-white shadow-lg rounded-md py-2 w-48">
+                            <Link to="/resources/buyers" className="px-4 py-2 hover:bg-gray-100">
+                                Buyer Resources
+                            </Link>
+                            <Link to="/resources/sellers" className="px-4 py-2 hover:bg-gray-100">
+                                Seller Resources
+                            </Link>
+                            <Link to="/resources/investors" className="px-4 py-2 hover:bg-gray-100">
+                                Investor Resources
+                            </Link>
+                        </div>
+                    )}
                 </div>
                 <Link to="/contact">Contact</Link>
             </div>
